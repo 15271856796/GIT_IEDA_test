e65374e728c7c9568e8cd1a1f699b74d870337b6
Administrator <15271856796@163.com>
2019-08-29 17:00:42 +0800

    添加了一个输出语句

---
+++ src/com/hdl/java/HelloWord.java
@@ -0,0 +1,10 @@
+package com.hdl.java;
+
+public class HelloWord {
+    public static void main(String[] args){
+        System.out.println("helloword");
+        System.out.println("1111");
+        System.out.println("2222");
+
+    }
+}
